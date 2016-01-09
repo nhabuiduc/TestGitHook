a68cec0fd348e8ab15d05b380e655172cb5a0406
Nha <hugodichoi@gmail.com>
2016-01-09 11:11:52 +0800

    t

--- DPBRWD_app/pre-push.js
+++ DPBRWD_app/pre-push.js
@@ -9,6 +9,35 @@ function execute(command, callback){
 
 execute("git cherry",function(data){
 	console.log(data);
+    
+    data = data.replace(/\+/g,"").replace(/ /g,"");
+    var commits = data.split('\n');
+    
+    var allFileChanges = []
+    var count = commits.length;
+    
+    commits.forEach(function(commit){
+        var cmd = 'git diff-tree --no-commit-id --name-only -r '+commit;
+        if(commit == "") return;
+        execute(cmd, function(commitFileChanges){
+            addDistinct(allFileChanges,commitFileChanges.split('\n'));
+            count--; 
+            if(count == 0){
+                process.exit(1);
+                console.log(allFileChanges);
+            }                       
+        })  
+    })
+    
+            
 })
 
-process.exit(1);
+function addDistinct(sourceArr, destArr){
+    destArr.forEach(function(item){
+        if(sourceArr.indexOf(item) >=0 ) return;
+        sourceArr.push(item);
+    })
+}
+
+
+
