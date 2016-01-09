86a80ec3826011840d2485f5f7d84f4f6fee0e92
Nha <hugodichoi@gmail.com>
2016-01-09 11:33:02 +0800

    t

--- DPBRWD_app/pre-push.js
+++ DPBRWD_app/pre-push.js
@@ -7,25 +7,36 @@ function execute(command, callback){
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
 execute("git cherry",function(data){
-	console.log(data);
+	
     
     data = data.replace(/\+/g,"").replace(/ /g,"");
     
-    var commits = data.split('\n');
+    var commits = data.split('\n').clean("");
     
     var allFileChanges = []
     var count = commits.length;
-    
+    console.log(commits);
     commits.forEach(function(commit){
         var cmd = 'git diff-tree --no-commit-id --name-only -r '+commit;
-        if(commit == "") return;
-        execute(cmd, function(commitFileChanges){
-            addDistinct(allFileChanges,commitFileChanges.split('\n'));
+        
+        execute(cmd, function(commitFileChanges){            
+            addDistinct(allFileChanges,commitFileChanges.split('\n').clean(""));
             count--; 
             if(count == 0){
-                process.exit(1);
                 console.log(allFileChanges);
+                process.exit(1);
+                
             }                       
         })  
     })
@@ -33,8 +44,11 @@ execute("git cherry",function(data){
             
 })
 
+
+
 function addDistinct(sourceArr, destArr){
     destArr.forEach(function(item){
+        
         if(sourceArr.indexOf(item) >=0 ) return;
         sourceArr.push(item);
     })
