1a35cad32db4bb77d613f7151e9a487f8364ffcf
Nha <hugodichoi@gmail.com>
2016-01-10 11:29:55 +0800

    a

--- DPBRWD_app/bower-version-hook.js
+++ DPBRWD_app/bower-version-hook.js
@@ -1,170 +1,84 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
-
+var Promise = require("./bluebird");
+var _ = require("./lodash");
 var zeroVersion = "0.0.0";
 
 function BowerVersionHook() {
     var $this = this;
-    this.gitShowCmd = "git show HEAD:";
-    this.code = 0;
-    this.componentArr;
-    this.doneWithStatus = function (code) {
-        console.log('exit with code: ', code);
-        console.log($this.componentArr);
-        console.log("ERROR: please fix above errors")
-        process.exit(code);
-    }
-    this.getChangeFileChanges = function (callback) {
-        execute('git diff --cached --name-only', function (data) {
-            console.log(data);
-            var fileChanges = data.split('\n').clean("");
-            callback(fileChanges);
-        })
-    }
+    this.gitShowCmd = "git show HEAD:";    
+    this.getFileChanges = null;
 
     this.execute = function () {
-        this.getChangeFileChanges(hookWithFileChanges)
+        return this.getFileChanges().then(function (data) {
+            return hookWithFileChanges(data);
+        })
     }
 
     function hookWithFileChanges(fileChanges) {
-        var componentInfoArray = getChangedComponents(fileChanges);
-        $this.componentArr = componentInfoArray;
-        componentInfoArray = addBowerJsonFiles(componentInfoArray);
-        componentInfoArray = addMissingBowerJsonFiles(fileChanges, componentInfoArray);
-                
-        componentInfoArray = addBowerVersion(componentInfoArray);
-        
-        checkAllBowerVersions(componentInfoArray);
-    }    
 
-    function done() {
-        $this.doneWithStatus($this.code);
+        var componentInfoPromises =
+            _.chain(fileChanges)
+                .map(function (path) { return path.split('/').clean(""); })
+                .filter(function (pathSplit) { return pathSplit.length >= 3 && pathSplit[1] === 'components' })
+                .map(function (pathSplit) { return pathSplit[2] })
+                .uniq()
+                .map(function (componentName) { return mapToComponentInfo(componentName, fileChanges) })
+                .map(function (componentInfo) {
+                    return checkWithPreviousBowerVersion(componentInfo);
+                })                
+                .value();
+
+        return Promise.all(componentInfoPromises);
     }
 
-    function checkAllBowerVersions(bowerInfoArr) {	
-        // console.log(bowerInfoArr);
-        var count = bowerInfoArr.length;
-
-        var arr = [];
-        bowerInfoArr.forEach(function (bowerInfo) {
-            var cmd = $this.gitShowCmd + bowerInfo.bowerJsonFile;
-            execute(cmd, function (bowerJson) {
-
-                bowerInfo.previousBowerVersion = getBowerJsonVersion(bowerJson);
-                arr.push(bowerInfo);
-                count--;
-                if (count == 0) {
-                    processAllBowerInfo(arr);
-                }
-
-            })
-        })
-    }
-
-    function processAllBowerInfo(bowerInfoArray) {
-
-        var wrongVersionComponents = []
-        bowerInfoArray.forEach(function (bowerInfo) {
-            if (!checkVersionHasIncreased(bowerInfo.bowerVersion, bowerInfo.previousBowerVersion)) {
-                wrongVersionComponents.push(bowerInfo);
-            }
-        })
-
-        if (wrongVersionComponents.length > 0) {
-            console.log("These components did not increase version number correctly")
-            wrongVersionComponents.forEach(function (info) {
-                console.log("Component: ", info.componentName, ", previous: ", info.previousBowerVersion, ", current: ", info.bowerVersion);
-            })
-
-            setCodeStatus(1);
-            
-        }else {
-            setCodeStatus(0);
+    function mapToComponentInfo(componentName, fileChanges) {
+        var bowerJsonFile = 'DPBRWD_app/components/' + componentName + '/bower.json';
+        var bowerVersion = zeroVersion;
+        if (fs.existsSync(bowerJsonFile)) {
+            bowerVersion = getBowerJsonVersion(fs.readFileSync(bowerJsonFile, 'utf8'));
         }
-        
-        done();
-        
+        return {
+            componentName: componentName,
+            bowerJsonFile: bowerJsonFile,
+            missingJsonChanged: fileChanges.indexOf(bowerJsonFile) < 0,
+            bowerVersion: bowerVersion
+        }
     }
 
-    function setError(msg) {
-        console.log(msg);
-        setCodeStatus(1);
-    }
-
-    function setCodeStatus(code) {
-        $this.code = code;              
-        //process.exit(code);
-    }
+    function checkWithPreviousBowerVersion(componentInfo) {
+        var cmd = $this.gitShowCmd + componentInfo.bowerJsonFile;
+        return new Promise(function (resolve, reject) {
+            execute(cmd, function (bowerJson) {
+                var previousBowerVersion = getBowerJsonVersion(bowerJson);
+                resolve(_.assign(componentInfo, {
+                    previousBowerVersion: previousBowerVersion,
+                    versionIncreased: checkVersionHasIncreased(componentInfo.bowerVersion, previousBowerVersion)
+                }));
+            })
+        })
+    }        
 }
 
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {         
-      this.splice(i, 1);
-      i--;
+Array.prototype.clean = function (deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == deleteValue) {
+            this.splice(i, 1);
+            i--;
+        }
     }
-  }
-  return this;
+    return this;
 };
 
 function execute(command, callback) {
     exec(command, function (error, stdout, stderr) { callback(stdout); });
 };
 
-function getChangedComponents(names) {
-    var componentInfoArray = [];
-    var componentNames = [];
-    names.forEach(function (value) {
-        var split = value.split('/').clean("");
-
-        if (split.length >= 3 && split[1] === 'components') {
-            var componentName = split[2];
-            if (componentNames.indexOf(componentName) < 0) {
-                componentInfoArray.push({
-                    componentName: componentName
-                });
-            }
-            componentNames.push(componentName);
-        }
-    })
-
-    return componentInfoArray;
-
-}
-
-
-function addBowerJsonFiles(componentInfoArray) {
-    componentInfoArray.forEach(function (componentInfo) {
-        componentInfo.bowerJsonFile = 'DPBRWD_app/components/' + componentInfo.componentName + '/bower.json';
-    })
-
-    return componentInfoArray;
-}
-
-function addMissingBowerJsonFiles(fileChanges, componentInfoArray) {
-
-    componentInfoArray.forEach(function (componentInfo) {
-        componentInfo.missingJsonChanged = fileChanges.indexOf(componentInfo.bowerJsonFile) < 0;
-    })
-
-    return componentInfoArray;
-}
 
 function getBowerJsonVersion(json) {
     if (!json) return zeroVersion;
     var obj = JSON.parse(json);
     return obj.version || zeroVersion;
-}
-
-function addBowerVersion(componentInfoArray) {
-
-    componentInfoArray.forEach(function (info) {
-        info.bowerVersion = zeroVersion;
-        if (fs.existsSync(info.bowerJsonFile)) {
-            info.bowerVersion = getBowerJsonVersion(fs.readFileSync(info.bowerJsonFile, 'utf8'));
-        }
-    })
-    return componentInfoArray;
 }
 
 function checkVersionHasIncreased(currBowerVersion, prevBowerVersion) {
@@ -183,8 +97,6 @@ function checkVersionHasIncreased(currBowerVersion, prevBowerVersion) {
 
     return false;
 }
-
-
 
 module.exports = BowerVersionHook;
 global.execute = execute;
--- DPBRWD_app/pre-commit.js
+++ DPBRWD_app/pre-commit.js
@@ -1,4 +1,14 @@
+/* global execute */
 var BowerVersionHook = require("./bower-version-hook");
 
 var  hook = new BowerVersionHook();
-hook.execute();
+hook.getFileChanges = function () {
+        return new Promise(function (resolve, reject) {
+            execute('git diff --cached --name-only', function (data) {
+                resolve(data.split('\n').clean(""));
+            })
+        })
+    }
+hook.execute().then(function(arr){
+    console.log(arr);
+});
